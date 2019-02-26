86df24a3f778a4c6c2376fb052ac195a0ac46409
sirdude103 <sirdude103@gmail.com>
2019-02-25 21:47:40 -0600

    Removed dev-only CAS lines

--- CoSaM-Purchase-Form-System/php/CAS_authentication.php
+++ CoSaM-Purchase-Form-System/php/CAS_authentication.php
@@ -5,13 +5,6 @@ include_once('../phpCAS-master/CAS.php');
 
 $client = phpCAS::client(CAS_VERSION_3_0,'arachnid-devel.ndsu.nodak.edu',443,'/cas/');
 
-//For development. Prints out additional warnings. 
-phpCAS::setVerbose(true);
-
-// REMOVE THIS AFTER DEVELOPMENT
-phpCAS::setNoCasServerValidation();
-//phpCAS::setCasServerCACert('arachnid-devel.ndsu.nodak.edu/cas/p3/serviceValidate', true);
-
 if(!phpCAS::isAuthenticated()) {
 	phpCAS::forceAuthentication();
 }
