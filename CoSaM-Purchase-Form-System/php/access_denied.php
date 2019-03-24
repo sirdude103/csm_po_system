66df8767a6fce376047a1526f4ff3faeae1afe11
sirdude103 <sirdude103@gmail.com>
2019-03-24 17:35:24 -0500

    Readjusted login to allow all NDSU employees access (CAS members with employees@ndsu.edu status)

---
+++ CoSaM-Purchase-Form-System/php/access_denied.php
@@ -0,0 +1,22 @@
+﻿<?php
+####################
+# Name: PHP access_denied.php
+# Description: Redirect site for CAS members who are not employees at NDSU
+# Initial Creation Date: 03/24/2019
+# Last Modification Date: 03/24/2019
+# Author: Wyly Andrews
+####################
+
+?>
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title></title>
+</head>
+<body>
+    You are not allowed to access this site. You must be an employee at NDSU to gain access.
+</body>
+</html>
+
