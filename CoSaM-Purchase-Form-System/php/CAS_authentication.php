4486d6286c02746be4933ce28e721e0beb39dbe6
sirdude103 <sirdude103@gmail.com>
2019-04-07 21:54:55 -0500

    different CAS destination

--- CoSaM-Purchase-Form-System/php/CAS_authentication.php
+++ CoSaM-Purchase-Form-System/php/CAS_authentication.php
@@ -3,7 +3,7 @@
 include_once('../phpCAS-master/CAS.php');
 
 
-$client = phpCAS::client(CAS_VERSION_3_0,'arachnid-devel.ndsu.nodak.edu',443,'/cas/');
+$client = phpCAS::client(CAS_VERSION_3_0,'apps.ndsu.edu',443,'/cas/');
 
 if(!phpCAS::isAuthenticated()) {
 	phpCAS::forceAuthentication();
