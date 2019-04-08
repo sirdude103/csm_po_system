9ba5cd6f58028a8b9d0e9fd3fd0571531b4a0c3f
sirdude103 <sirdude103@gmail.com>
2019-04-08 17:48:21 -0500

    Setting up CAS for production (WIP)

--- CoSaM-Purchase-Form-System/php/CAS_authentication.php
+++ CoSaM-Purchase-Form-System/php/CAS_authentication.php
@@ -2,8 +2,14 @@
 
 include_once('../phpCAS-master/CAS.php');
 
-
 $client = phpCAS::client(CAS_VERSION_3_0,'apps.ndsu.edu',443,'/cas/');
+
+phpCAS::setServerServiceValidateURL('https://apps.ndsu.edu/cas/p3/serviceValidate');
+phpCAS::setFixedServiceURL('https://www.ndsu.edu/pubweb/csm-po-system/php/home.php');
+
+//phpCAS::setServerLoginURL('https://apps.ndsu.edu/cas/login');
+//phpCAS::setFixedCallbackURL('https://www.ndsu.edu/pubweb/csm-po-system/');
+
 
 if(!phpCAS::isAuthenticated()) {
 	phpCAS::forceAuthentication();
